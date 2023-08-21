89f73f9fa64ad19412760e1e63745f3e1d69b3dc
linajin03 <53534844+linajin03@users.noreply.github.com>
2023-08-20 22:35:25 -0400

    fixed mobile and added hamburger

--- index.js
+++ index.js
@@ -16,3 +16,12 @@ function myFunction() {
     }
 }
 
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function topNav() {
+    var x = document.getElementById("nav-links");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
