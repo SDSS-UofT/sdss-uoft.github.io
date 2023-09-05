2b5a285b3291a961af2d68a72930871379b5f45d
Chin Chin Jim <chinchin.jim@mail.utoronto.ca>
2023-09-04 21:26:12 -0400

    added scrolling animations to fix awkward spacing between sections

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var header = document.getElementById("navBar");
 var sticky = header.offsetTop;
 
 // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
+function makeNavSticky() {
     if (window.scrollY > sticky) {
         header.classList.add("sticky");
     } else {
@@ -14,7 +14,26 @@ function myFunction() {
 }
 
 // When the user scrolls the page, execute myFunction
-window.addEventListener("scroll", myFunction);
+window.addEventListener("scroll", makeNavSticky);
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 175;
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+
+window.addEventListener("scroll", reveal);
+
+// To check the scroll position on page load
+reveal();
 
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
 function topNav() {
