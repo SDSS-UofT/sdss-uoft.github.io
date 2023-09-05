110ac3284b99c7875541d5290318f77086523576
Chin Chin Jim <chinchin.jim@mail.utoronto.ca>
2023-09-04 20:35:58 -0400

    fixed navbar sticky and added functionality to join us buttons

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
-
 // Get the header
 var header = document.getElementById("navBar");
 
@@ -9,12 +6,15 @@ var sticky = header.offsetTop;
 
 // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
-    if (window.pageYOffset > sticky) {
+    if (window.scrollY > sticky) {
         header.classList.add("sticky");
     } else {
         header.classList.remove("sticky");
     }
 }
+
+// When the user scrolls the page, execute myFunction
+window.addEventListener("scroll", myFunction);
 
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
 function topNav() {
