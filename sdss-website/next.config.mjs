68239fbd5f9ba2dfdf332edb1e2daab7129c5612
Lina Jin <53534844+linajin03@users.noreply.github.com>
2024-10-06 05:33:43 -0400

    Update next.config.mjs

--- sdss-website/next.config.mjs
+++ sdss-website/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: "/sdss-website",
+    output: "export",
+    reactStrictMode: true,
+};
 
 export default nextConfig;
