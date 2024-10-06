7982042056082a5ff306721d0f5374f172652a75
Lina Jin <53534844+linajin03@users.noreply.github.com>
2024-10-06 06:51:02 -0400

    Update ExecCard.tsx
    exec pfp smaller for mobile

--- sdss-website/src/app/components/exec-card/ExecCard.tsx
+++ sdss-website/src/app/components/exec-card/ExecCard.tsx
@@ -15,7 +15,7 @@ const Card: React.FC<Exec> = ({ name, role, year, program, linkedin, pfp }) => {
     <div className="bg-white pink-border lg:card-width card-mobile w-full lg:mt-0 lg:mt-4 flex flex-row items-center">
       {/* Profile picture */}
       {pfp ? (
-        <img src={pfp} alt={name} className="lg:w-40 lg:h-40 w-28 h-28 m-4 rounded-full object-cover shrink-0" />
+        <img src={pfp} alt={name} className="lg:w-40 lg:h-40 w-24 h-24 m-4 rounded-full object-cover shrink-0" />
       ) : (
         <div className="w-40 h-40 rounded-full object-cover bg-gray-200 shrink-0"></div>
       )}
