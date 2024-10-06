d75d949901ac761a94b03b10c644db09ec77f18d
linajin03 <53534844+linajin03@users.noreply.github.com>
2024-10-06 13:45:24 -0400

    fixing hamburger

--- sdss-website/src/app/components/top-nav/page.tsx
+++ sdss-website/src/app/components/top-nav/page.tsx
@@ -66,7 +66,7 @@ const TopNavbar = () => {
                 <svg
                   className="w-6 h-6"
                   fill="none"
-                  stroke="currentColor"
+                  stroke="white"
                   viewBox="0 0 24 24"
                   xmlns="http://www.w3.org/2000/svg"
                 >
@@ -81,7 +81,7 @@ const TopNavbar = () => {
                 <svg
                   className="w-6 h-6"
                   fill="none"
-                  stroke="currentColor"
+                  stroke="white"
                   viewBox="0 0 24 24"
                   xmlns="http://www.w3.org/2000/svg"
                 >
