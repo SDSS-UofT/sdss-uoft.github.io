69ea7926c53071cfe30e2ae30369de43d6ffb143
Eiqan <117050195+eiqanahmed@users.noreply.github.com>
2025-11-10 12:51:35 -0500

    Update executive team data for new team members

--- sdss-website/src/app/data/presidents.ts
+++ sdss-website/src/app/data/presidents.ts
@@ -3,20 +3,44 @@ export const presidentData =
 [
     {
         "id": 0,
-        "name": "Rustam Atahodja",
-        "role": "Co-President",
+        "name": "Rahil Chadha",
+        "role": "President",
         "year": "4th Year",
-        "program": "Statistics and Economics Majors (Data Analytics Focus), Mathematics Minor",
-        "linkedin": "https://www.linkedin.com/in/rustam-atahodja/",
-        "pfp": "pfps/rustama.png",
+        "program": "Statistics and Economics Major",
+        "linkedin": "https://www.linkedin.com/in/rahil-chadha-7109941b2/",
+        "pfp": "pfps-2025-2026/rahilc.png",
     },
     {
         "id": 1,
-        "name": "Lina Jin",
-        "role": "Co-President",
+        "name": "Raghav Sinha",
+        "role": "Executive VP",
         "year": "4th Year",
-        "program": "Computer Science and Statistics Majors",
-        "linkedin": "https://www.linkedin.com/in/jin-lina/",
-        "pfp": "pfps/linaj.png",
+        "program": "Economics & Statistics Majors, Computer Science Minor",
+        "linkedin": "https://www.linkedin.com/in/raghav-sinha/",
+        "pfp": "pfps-2025-2026/raghav_s.jpeg",
     },
 ]
+
+
+// 2024 - 2025 TEAM DATA:
+// export const presidentData =
+// [
+//     {
+//         "id": 0,
+//         "name": "Rustam Atahodja",
+//         "role": "Co-President",
+//         "year": "4th Year",
+//         "program": "Statistics and Economics Majors (Data Analytics Focus), Mathematics Minor",
+//         "linkedin": "https://www.linkedin.com/in/rustam-atahodja/",
+//         "pfp": "pfps/rustama.png",
+//     },
+//     {
+//         "id": 1,
+//         "name": "Lina Jin",
+//         "role": "Co-President",
+//         "year": "4th Year",
+//         "program": "Computer Science and Statistics Majors",
+//         "linkedin": "https://www.linkedin.com/in/jin-lina/",
+//         "pfp": "pfps/linaj.png",
+//     },
+// ]
