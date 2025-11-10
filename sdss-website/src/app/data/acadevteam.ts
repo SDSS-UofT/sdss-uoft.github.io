532fe8d1fc65e74d4dab3f6f0d33f2c018d67563
Eiqan <117050195+eiqanahmed@users.noreply.github.com>
2025-11-10 12:20:45 -0500

    Update acaDevTeamData with new team members

--- sdss-website/src/app/data/acadevteam.ts
+++ sdss-website/src/app/data/acadevteam.ts
@@ -1,40 +1,95 @@
-
 export const acaDevTeamData =
 [
     {
         "id": 0,
-        "name": "Nick Pestov",
-        "role": "VP of Academic Development",
-        "year": "4th Year",
-        "program": "Computer Science Specialist, Statistics Major, HPS Minor",
-        "linkedin": "https://www.linkedin.com/in/nick-pestov/",
-        "pfp": "pfps/nickp.png",
+        "name": "Jingwen (Lisa) Zhong",
+        "role": "Academic Development Outreach Director",
+        "year": "ASIP Year",
+        "program": "Data Science Specialist, Actuarial Science Major",
+        "linkedin": "https://www.linkedin.com/in/jingwenzhong/",
+        "pfp": "pfps-2025-2026/lisa_z.jpeg",
     },
+
     {
         "id": 1,
-        "name": "Christie Ko",
-        "role": "Director of Outreach",
-        "year": "2nd Year",
-        "program": "Economics and Statistics Majors",
-        "linkedin": "https://www.linkedin.com/in/christie-k-9734a9217/",
-        "pfp": "pfps/christiek.png",
+        "name": "Richard Hong",
+        "role": "VP of Academic Development",
+        "year": "4th Year",
+        "program": "Finance and Economics Specialist, Statistics Major",
+        "linkedin": "https://www.linkedin.com/in/richard-hong-8685a0270/",
+        "pfp": ""
     },
+
     {
         "id": 2,
-        "name": "Richard Hong",
-        "role": "Co-Director of Logistics",
-        "year": "3rd Year",
-        "program": "Finance and Economics Specialist, Statistics and Economics Majors",
-        "linkedin": "",
-        "pfp": "pfps/richardh.png",
+        "name": "Xiaotong Shen",
+        "role": "Academic Development Outreach Director",
+        "year": "2nd Year",
+        "program": "Data Science specialist + Computer Science major",
+        "linkedin": "https://www.linkedin.com/in/shen-xiaotong/",
+        "pfp": "pfps-2025-2026/xiaotong_s.jpeg"
     },
+
     {
         "id": 3,
-        "name": "Nikita Jain",
-        "role": "Co-Director of Logistics",
-        "year": "2nd Year",
-        "program": "Economics and Statistics Majors (Data Analytics Focus), Mathematics Minor",
-        "linkedin": "https://www.linkedin.com/in/nikita05/",
-        "pfp": "pfps/nikitaj.png",
+        "name": "Tanmay Shinde",
+        "role": "Academic Development Outreach Director",
+        "year": "4th Year",
+        "program": "CS Spec, Stats Major",
+        "linkedin": "https://www.linkedin.com/in/tanmay-shinde-054142239/",
+        "pfp": "pfps-2025-2026/tanmay_s.jpeg"
     },
+
+    {
+        "id": 4,
+        "name": "Jean Jung",
+        "role": "Academic Development Logistics Director",
+        "year": "3rd Year",
+        "program": "Bioinformatics Specialist, CS Major",
+        "linkedin": "https://www.linkedin.com/in/tanmay-shinde-054142239/",
+        "pfp": "pfps-2025-2026/jean_j.jpeg"
+    }
 ]
+
+
+// 2024 - 2025 TEAM DATA:
+// export const acaDevTeamData =
+// [
+//     {
+//         "id": 0,
+//         "name": "Nick Pestov",
+//         "role": "VP of Academic Development",
+//         "year": "4th Year",
+//         "program": "Computer Science Specialist, Statistics Major, HPS Minor",
+//         "linkedin": "https://www.linkedin.com/in/nick-pestov/",
+//         "pfp": "pfps/nickp.png",
+//     },
+//     {
+//         "id": 1,
+//         "name": "Christie Ko",
+//         "role": "Director of Outreach",
+//         "year": "2nd Year",
+//         "program": "Economics and Statistics Majors",
+//         "linkedin": "https://www.linkedin.com/in/christie-k-9734a9217/",
+//         "pfp": "pfps/christiek.png",
+//     },
+//     {
+//         "id": 2,
+//         "name": "Richard Hong",
+//         "role": "Co-Director of Logistics",
+//         "year": "3rd Year",
+//         "program": "Finance and Economics Specialist, Statistics and Economics Majors",
+//         "linkedin": "",
+//         "pfp": "pfps/richardh.png",
+//     },
+//     {
+//         "id": 3,
+//         "name": "Nikita Jain",
+//         "role": "Co-Director of Logistics",
+//         "year": "2nd Year",
+//         "program": "Economics and Statistics Majors (Data Analytics Focus), Mathematics Minor",
+//         "linkedin": "https://www.linkedin.com/in/nikita05/",
+//         "pfp": "pfps/nikitaj.png",
+//     },
+// ]
+
