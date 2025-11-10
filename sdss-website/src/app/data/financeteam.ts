4a3cd53f44fb7409f67e3d09ebf28374b24ae14f
Eiqan <117050195+eiqanahmed@users.noreply.github.com>
2025-11-10 12:58:52 -0500

    Update finance team data for 2025-2026

--- sdss-website/src/app/data/financeteam.ts
+++ sdss-website/src/app/data/financeteam.ts
@@ -2,29 +2,52 @@ export const financeTeamData =
 [
     {
         "id": 0,
-        "name": "Tim Koukarine",
-        "role": "VP of Finance",
-        "year": "4th year",
+        "name": "Avery Jun",
+        "role": "Director of Finance",
+        "year": "3rd Year",
         "program": "Finance & Economics Specialist",
-        "linkedin": "",
-        "pfp": "pfps/timk.png",
+        "linkedin": "https://www.linkedin.com/in/avery-jun-8a163a238/",
+        "pfp": "pfps-2025-2026/avery_j.jpeg",
     },
     {
         "id": 1,
-        "name": "Shirley Chen",
-        "role": "Co-Director of Partnerships",
-        "year": "4th year",
-        "program": "Bachelor of Information",
-        "linkedin": "",
-        "pfp": "pfps/shirleyc.png",
+        "name": "Aditya Pardeshi",
+        "role": "VP of Finance",
+        "year": "4th Year",
+        "program": "Economics major with a double minor in Statistics and Mathematics",
+        "linkedin": "https://www.linkedin.com/in/aditya-pardeshi-uoft/",
+        "pfp": "pfps-2025-2026/aditya_p.jpeg",
     },
-    {
-        "id": 2,
-        "name": "Annabelle Topas",
-        "role": "Co-Director of Partnerships",
-        "year": "3rd year",
-        "program": "Industrial Engineering",
-        "linkedin": "",
-        "pfp": "pfps/annabellet.png",
-    }
 ]
+
+// 2024 - 2025 TEAM DATA:
+// export const financeTeamData =
+// [
+//     {
+//         "id": 0,
+//         "name": "Tim Koukarine",
+//         "role": "VP of Finance",
+//         "year": "4th year",
+//         "program": "Finance & Economics Specialist",
+//         "linkedin": "",
+//         "pfp": "pfps/timk.png",
+//     },
+//     {
+//         "id": 1,
+//         "name": "Shirley Chen",
+//         "role": "Co-Director of Partnerships",
+//         "year": "4th year",
+//         "program": "Bachelor of Information",
+//         "linkedin": "",
+//         "pfp": "pfps/shirleyc.png",
+//     },
+//     {
+//         "id": 2,
+//         "name": "Annabelle Topas",
+//         "role": "Co-Director of Partnerships",
+//         "year": "3rd year",
+//         "program": "Industrial Engineering",
+//         "linkedin": "",
+//         "pfp": "pfps/annabellet.png",
+//     }
+// ]
