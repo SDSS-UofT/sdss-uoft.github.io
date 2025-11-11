af3486909006a4212b16f9759ed86066d295b268
Eiqan <117050195+eiqanahmed@users.noreply.github.com>
2025-11-11 15:09:14 -0500

    Add synergy team data to synergyteam.ts
    Added synergy team data including members' details.

--- sdss-website/src/app/data/synergyteam.ts
+++ sdss-website/src/app/data/synergyteam.ts
@@ -1,1 +1,39 @@
-
+export const synergyTeamData =
+[
+  {
+    "id": 0,
+    "name": "Jake Cohen",
+    "role": "Director of Logistics",
+    "year": "3rd Year",
+    "program": "Major in Biodiversity and Conservation Biology, Major in Statistics",
+    "linkedin": "https://www.linkedin.com/in/jake-e-cohen/",
+    "pfp": "pfps-2025-2026/jake_c.jpeg",
+  },
+  {
+    "id": 1,
+    "name": "Sami Ahmed",
+    "role": "Mentorship Engagement and Outreach Director",
+    "year": "4th Year",
+    "program": "Mathematics and its Applications Specialist (Probability/Statistics), Philosophy (Minor)",
+    "linkedin": "https://www.linkedin.com/in/samiahmedca/",
+    "pfp": "pfps-2025-2026/sami_a.png",
+  },
+  {
+    "id": 2,
+    "name": "Yolanda Thant",
+    "role": "VP of Mentorship",
+    "year": "Co-op Year",
+    "program": "Computer Science Specialist, Statistical Sciences Specialist",
+    "linkedin": "https://www.linkedin.com/in/yolandathant/",
+    "pfp": "pfps-2025-2026/yolanda_t.jpeg",
+  },
+  {
+    "id": 3,
+    "name": "Nikita Jain",
+    "role": "Director of Synergy",
+    "year": "3rd Year",
+    "program": "Economics and Statistics Majors and Math Minor",
+    "linkedin": "https://www.linkedin.com/in/nikita05/",
+    "pfp": "pfps-2025-2026/nikita_j.png",
+  }  
+]
