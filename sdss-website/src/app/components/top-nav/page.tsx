f35ac1868232fa7bd8d99b5cdc66554167f96a52
Jennifer <jjennifer.huang@mail.utoronto.ca>
2026-03-15 22:33:13 -0400

    fix: fixed colour of nav bar on thanks page

--- sdss-website/src/app/components/top-nav/page.tsx
+++ sdss-website/src/app/components/top-nav/page.tsx
@@ -25,7 +25,7 @@ const TopNavbar = () => {
         className={`w-full h-20 top-0 ${
           isOpen
             ? "bg-dark-purple"
-            : pathname.startsWith("/events")
+            : pathname.startsWith("/events") || pathname.startsWith("/thanks") 
             ? "lg:bg-white bg-dark-purple bg-opacity-10"
             : "lg:bg-transparent lg:backdrop-blue-none bg-dark-purple bg-opacity-10"
         } transition-colors duration-300`}
